47b8d5652cea1c3e66b086cafe4dc98e83ec1835
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-06-30 09:12:58 +0300

    add base player class

---
+++ BasePlayer.h
@@ -0,0 +1,16 @@
+//
+// Created by ozgur on 6/30/2023.
+//
+
+#ifndef INC_21BLACKJACK_BASEPLAYER_H
+#define INC_21BLACKJACK_BASEPLAYER_H
+
+
+
+class BasePlayer {
+
+};
+
+
+
+#endif //INC_21BLACKJACK_BASEPLAYER_H
